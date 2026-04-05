58aa19fb34a1dcd1790ae21fa48af6bff4e5de41
Divya P <divyadily2903@gmail.com>
2026-04-05 19:41:52 +0530

    Add files via upload

--- User.js
+++ User.js
@@ -4,11 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: {
-        type: String,
-        enum: ['User', 'Admin'],
-        default: 'User',
-    },
+    role: { type: String, default: 'User' }, 
     createdAt: { type: Date, default: Date.now }
 });
 
